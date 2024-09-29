86c500d76f01e7cdadd4c3682505190619498812
dabelyantseva <dabelyantseva@ozon.ru>
2024-09-29 22:04:21 +0300

    add doxygen support

--- src/sort.cpp
+++ src/sort.cpp
@@ -4,6 +4,13 @@
 
 namespace algo {
 
+/**
+ * \brief Sort integers.
+ *
+ * This function takes vector of integer numbers and sorts it inplace.
+ * 
+ * \param v Vector of integers for sort.
+ */
 void Sort(std::vector<int>& v) {
   int n = v.size();
 
